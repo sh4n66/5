63454a5592362bde8b92e36ecce0f476ddff45c0
sh4n66 <boteusefor6@gmail.com>
2025-09-22 23:29:45 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -1,7 +1,7 @@
 import logging
-from struct import pack
 import re
 import base64
+from struct import pack
 from pyrogram.file_id import FileId
 from pymongo.errors import DuplicateKeyError
 from umongo import Instance, Document, fields
@@ -12,15 +12,14 @@ from info import DATABASE_URI, DATABASE_NAME, COLLECTION_NAME, USE_CAPTION_FILTE
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-# MongoDB connection
+# Connect to MongoDB
 client = AsyncIOMotorClient(DATABASE_URI)
 db = client[DATABASE_NAME]
 
-# Use umongo Instance (old API)
-instance = Instance.from_db(db)
+# For umongo >= 3.x
+instance = Instance(db)
 
 
-# Document model
 @instance.register
 class Media(Document):
     file_id = fields.StrField(attribute='_id')
@@ -32,16 +31,19 @@ class Media(Document):
     caption = fields.StrField(allow_none=True)
 
     class Meta:
-        indexes = ('$file_name', )
         collection_name = COLLECTION_NAME
+        indexes = ('$file_name',)
 
 
-# Save file in database
+# -----------------------------
+# Database utility functions
+# -----------------------------
+
 async def save_file(media):
     """Save file in database"""
-
     file_id, file_ref = unpack_new_file_id(media.file_id)
     file_name = re.sub(r"(_|\-|\.|\+)", " ", str(media.file_name))
+
     try:
         file = Media(
             file_id=file_id,
@@ -53,105 +55,65 @@ async def save_file(media):
             caption=media.caption.html if media.caption else None,
         )
     except ValidationError:
-        logger.exception('Error occurred while saving file in database')
+        logger.exception("Error occurred while saving file in database")
         return False, 2
+
+    try:
+        await file.commit()
+    except DuplicateKeyError:
+        logger.warning(f"{getattr(media, 'file_name', 'NO_FILE')} is already saved in database")
+        return False, 0
     else:
-        try:
-            await file.commit()
-        except DuplicateKeyError:      
-            logger.warning(
-                f'{getattr(media, "file_name", "NO_FILE")} is already saved in database'
-            )
-            return False, 0
-        else:
-            logger.info(f'{getattr(media, "file_name", "NO_FILE")} is saved to database')
-            return True, 1
+        logger.info(f"{getattr(media, 'file_name', 'NO_FILE')} is saved to database")
+        return True, 1
 
 
-# Search results
-async def get_search_results(query, file_type=None, max_results=(MAX_BTN), offset=0, filter=False):
-    """For given query return (results, next_offset)"""
-
+async def get_search_results(query, file_type=None, max_results=MAX_BTN, offset=0):
+    """For given query return (results, next_offset, total_results)"""
     query = query.strip()
+
     if not query:
-        raw_pattern = '.'
-    elif ' ' not in query:
-        raw_pattern = r'(\b|[\.\+\-_])' + query + r'(\b|[\.\+\-_])'
+        raw_pattern = "."
+    elif " " not in query:
+        raw_pattern = r"(\b|[\.\+\-_])" + query + r"(\b|[\.\+\-_])"
     else:
-        raw_pattern = query.replace(' ', r'.*[\s\.\+\-_]')
+        raw_pattern = query.replace(" ", r".*[\s\.\+\-_]")
 
     try:
         regex = re.compile(raw_pattern, flags=re.IGNORECASE)
-    except:
-        return []
+    except re.error:
+        return [], "", 0
 
     if USE_CAPTION_FILTER:
-        filter = {'$or': [{'file_name': regex}, {'caption': regex}]}
+        filter_q = {"$or": [{"file_name": regex}, {"caption": regex}]}
     else:
-        filter = {'file_name': regex}
+        filter_q = {"file_name": regex}
 
     if file_type:
-        filter['file_type'] = file_type
+        filter_q["file_type"] = file_type
 
-    total_results = await Media.count_documents(filter)
+    total_results = await Media.count_documents(filter_q)
     next_offset = offset + max_results
     if next_offset > total_results:
-        next_offset = ''
+        next_offset = ""
 
-    cursor = Media.find(filter)
-    cursor.sort('$natural', -1)
-    cursor.skip(offset).limit(max_results)
+    cursor = Media.find(filter_q).sort("$natural", -1).skip(offset).limit(max_results)
     files = await cursor.to_list(length=max_results)
 
     return files, next_offset, total_results
 
 
-# Bad files
-async def get_bad_files(query, file_type=None, max_results=100, offset=0, filter=False):
-    """For given query return (results, next_offset)"""
-    query = query.strip()
-    if not query:
-        raw_pattern = '.'
-    elif ' ' not in query:
-        raw_pattern = r'(\b|[\.\+\-_])' + query + r'(\b|[\.\+\-_])'
-    else:
-        raw_pattern = query.replace(' ', r'.*[\s\.\+\-_]')
-
-    try:
-        regex = re.compile(raw_pattern, flags=re.IGNORECASE)
-    except:
-        return []
-
-    if USE_CAPTION_FILTER:
-        filter = {'$or': [{'file_name': regex}, {'caption': regex}]}
-    else:
-        filter = {'file_name': regex}
-
-    if file_type:
-        filter['file_type'] = file_type
-
-    total_results = await Media.count_documents(filter)
-    next_offset = offset + max_results
-    if next_offset > total_results:
-        next_offset = ''
-
-    cursor = Media.find(filter)
-    cursor.sort('$natural', -1)
-    cursor.skip(offset).limit(max_results)
-    files = await cursor.to_list(length=max_results)
-
-    return files, next_offset, total_results
-
-
-# Get file details
 async def get_file_details(query):
-    filter = {'file_id': query}
-    cursor = Media.find(filter)
+    filter_q = {"file_id": query}
+    cursor = Media.find(filter_q)
     filedetails = await cursor.to_list(length=1)
     return filedetails
 
 
-# File encoding helpers
+# -----------------------------
+# Helpers for encoding/decoding
+# -----------------------------
+
 def encode_file_id(s: bytes) -> str:
     r = b""
     n = 0
@@ -179,7 +141,7 @@ def unpack_new_file_id(new_file_id):
             int(decoded.file_type),
             decoded.dc_id,
             decoded.media_id,
-            decoded.access_hash
+            decoded.access_hash,
         )
     )
     file_ref = encode_file_ref(decoded.file_reference)
